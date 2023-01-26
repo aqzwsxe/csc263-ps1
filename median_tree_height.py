6b449e38aeb101f391789644bf0c2ae736ebba59
aqzwsxe <103016653+aqzwsxe@users.noreply.github.com>
2023-01-26 13:23:55 -0500

    Update median_tree_height.py

--- median_tree_height.py
+++ median_tree_height.py
@@ -19,8 +19,8 @@ def median_tree_height(commands, middle) -> list:
     result = [middle]
     for i in commands[1:]:
         insert_value = int(i[7:])
-        l_r_heap = insert(middle, left_right_heap, insert_value)
-        middle = l_r_heap[2]
+        insert(middle, left_right_heap, insert_value)
+        middle = left_right_heap[2]
         result.append(middle)
     print(result)
     return result
@@ -32,38 +32,48 @@ def get_higher_middle(li: list) -> int:
 
 def max_heap_helper(ne: list, i: int): # the root is at ne[0]
     temp = 2 * i + 1
-    if temp >= len(ne):
-        return None
-    elif temp + 1 >= len(ne):
-        if ne[temp] > ne[i]:
-            ne[temp], ne[i] = ne[i], ne[temp]
+    if i < len(ne) and len(ne) != 0:
+        if temp < len(ne) and temp + 1 < len(ne): # it has both left and right child
+            if max(ne[temp], ne[temp+1]) > ne[0]:
+
+                if ne[temp] > ne[temp+ 1]:
+                    ne[0], ne[temp] = ne[temp], ne[0]
+                    return
+                else:
+                    ne[0], ne[temp + 1] = ne[temp+1], ne[0]
+                    return
+        if temp < len(ne):
+            if ne[temp] > ne[0]:
+                ne[temp], ne[0] = ne[0], ne[temp]
+                return
     else:
-        max_heap_helper(ne, temp)
-        max_heap_helper(ne, temp + 1)
-        if (max(ne[temp], ne[temp+1])) > ne[i]:
-            if ne[temp] > ne[temp+1]:
-                ne[i], ne[temp] = ne[temp], ne[i]
+        return None
+
+    max_heap_helper(ne, temp)
+    min_heap_helper(ne, temp + 1)
 
 
 def min_heap_helper(ne: list, i: int): # the root is at ne[0]
 
     temp = 2 * i + 1
-    if i < len(ne):
+    if i < len(ne) and len(ne) != 0:
         if temp < len(ne) and temp+1 < len(ne):  # it has both left and right child
             if min(ne[temp], ne[temp+1]) < ne[i]:
                 if ne[temp] < ne[temp+1]:
-                    ne[i], ne[temp] = ne[temp], ne[i]
+                    ne[0], ne[temp] = ne[temp], ne[0]
                     return
                 else:
-                    ne[i], ne[temp+1] = ne[temp+1], ne[i]
+                    ne[0], ne[temp+1] = ne[temp+1], ne[0]
                     return
 
         if temp < len(ne): # only have the left child
             if ne[temp] < ne[i]:
-                ne[temp], ne[i] = ne[i], ne[temp]
+                ne[temp], ne[0] = ne[0], ne[temp]
                 return
-        min_heap_helper(ne, temp)
-        min_heap_helper(ne, temp+1)
+    else:
+        return None
+    min_heap_helper(ne, temp)
+    min_heap_helper(ne, temp+1)
 
 
 def initialize(data: str, middle: int) -> list:
@@ -73,7 +83,7 @@ def initialize(data: str, middle: int) -> list:
     greater = []
     total = []
     for i in li: # O(n)
-
+        print(int(i))
         num = int(i)
         if num < middle:
             less.append(num)
@@ -99,15 +109,17 @@ def insert(median: int, r: list, insert_value: int) -> list:
     if insert_value < median:
         left_heap.append(insert_value)
         max_heap_helper(left_heap, 0)
+    elif insert_value == median:
+        right_heap.append(insert_value)
+        min_heap_helper(right_heap, 0)
     else:
         right_heap.append(insert_value)
         min_heap_helper(right_heap, 0)
 
     length = len(list(left_heap)) + len(list(right_heap)) + 1
 
-    if length % 2 == 0:
-        if len(left_heap) == len(right_heap):
-            print(left_heap[0] / 2)
+    if length % 2 == 0 and isinstance(median, int):
+        if len(left_heap) > len(right_heap):
 
             new_mid = round((median + left_heap[0])/2, 1)
         else:
@@ -119,11 +131,14 @@ def insert(median: int, r: list, insert_value: int) -> list:
 
         else:
             new_mid = right_heap.pop(0)
-    if median <= new_mid:
+    if median <= new_mid and isinstance(median, int):
         left_heap.append(median)
-    else:
+    elif isinstance(median, int):
         right_heap.append(median)
+    else:
+        pass
     median = new_mid
+    r[2] = new_mid
     max_heap_helper(left_heap, 0)
     min_heap_helper(right_heap, 0)
 
@@ -140,4 +155,13 @@ if __name__ == '__main__':
      'insert 19',
      'insert 3',
      ], 9)
+
+  assert [0] == median_tree_height(['initialize 0'], 0)
+
+  assert [4, 3.5, 3, 3] == median_tree_height(
+      ['initialize 1 2 3 4 5 6 7',
+       'insert 1',
+       'insert 2',
+       'insert 3',
+       ], 4)
 
